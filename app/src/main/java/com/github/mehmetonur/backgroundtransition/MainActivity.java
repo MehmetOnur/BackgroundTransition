e7fc7c1ae77a1b11c13b4a2ece6c38c6168a38a1
onur <onur.uzuner@nomadcommerce.com>
2015-11-14 17:28:41 +0200

    Add Material Colors

--- app/src/main/java/com/github/mehmetonur/backgroundtransition/MainActivity.java
+++ app/src/main/java/com/github/mehmetonur/backgroundtransition/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 /**
  * Created by mehmet onur on 14.11.2015.
@@ -20,10 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getName();
     private static final int NUM_PAGES = 5;
-
+    LinearLayout circles;
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
-
     private Integer[] colors = null;
     private ArgbEvaluator argbEvaluator = new ArgbEvaluator();
 
@@ -35,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
-//        mPager.setPageTransformer(true, new CrossFadePageTransformer());
-
         mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
@@ -50,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageSelected(int position) {
-
+                setIndicator(position);
             }
 
             @Override
@@ -60,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         setUpColors();
+        buildCircles();
 
     }
 
@@ -70,6 +70,37 @@ public class MainActivity extends AppCompatActivity {
         Integer Page4Bg = ContextCompat.getColor(getApplicationContext(), R.color.Page4Bg);
         Integer Page5Bg = ContextCompat.getColor(getApplicationContext(), R.color.Page5Bg);
         colors = new Integer[]{Page1Bg, Page2Bg, Page3Bg, Page4Bg, Page5Bg};
+    }
+
+    private void buildCircles() {
+        circles = LinearLayout.class.cast(findViewById(R.id.circles));
+
+        float scale = getResources().getDisplayMetrics().density;
+        int padding = (int) (5 * scale + 0.5f);
+
+        for (int i = 0; i < NUM_PAGES; i++) {
+            ImageView circle = new ImageView(this);
+            circle.setImageResource(R.drawable.page_circle);
+            circle.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            circle.setAdjustViewBounds(true);
+            circle.setPadding(padding, 0, padding, 0);
+            circles.addView(circle);
+        }
+
+        setIndicator(0);
+    }
+
+    private void setIndicator(int index) {
+        if (index < NUM_PAGES) {
+            for (int i = 0; i < NUM_PAGES; i++) {
+                ImageView circle = (ImageView) circles.getChildAt(i);
+                if (i == index) {
+                    circle.setColorFilter(ContextCompat.getColor(getApplicationContext(), android.R.color.holo_red_light));
+                } else {
+                    circle.setColorFilter(ContextCompat.getColor(getApplicationContext(), android.R.color.transparent));
+                }
+            }
+        }
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
